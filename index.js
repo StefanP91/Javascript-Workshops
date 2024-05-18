bba62ad5441a827e2cf9331d4545fdd4d0e03795
Stefan Panov <64032523+StefanP91@users.noreply.github.com>
2024-05-18 21:50:57 +0200

    Adding Workshop-1/Task-2

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-// Checking Age for Different Ticket Prices
+// Checking Day of the Week
 
-let age=61;
+let day = "Monday";
 
-if(age<=0){
-    console .log("You entered a incorrect age");
+if(day =="Monday"){
+    console.log("Start of the week");
 }
-else if(age<12){
-    console .log("Child ticket: $5");
+else if(day=="Wednesday"){
+    console .log("Midweek");
 }
-else if (age>=12 && age<18){
-    console .log("Teen ticket: $7");
-}
-else if (age>=18 && age<=60){
-    console .log("Adult ticket: $10");
+else if(day=="Friday"){
+    console .log("Almost weekend");
 }
 else{
-    console .log("Senior ticket: $10");
+    console .log("Just another day");
 }
