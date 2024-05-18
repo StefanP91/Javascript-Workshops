73b95fd65d856dda152e379593c65bb43674e244
Stefan Panov <64032523+StefanP91@users.noreply.github.com>
2024-05-18 22:07:34 +0200

    Adding Workshop-1/Task-8

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-// Checking Age for Different Ticket Prices
+//Determining Shipping Cost Based on Order Amount
 
-let age=61;
+let orderAmount = 100;
 
-if(age<=0){
-    console .log("You entered a incorrect age");
-}
-else if(age<12){
-    console .log("Child ticket: $5");
-}
-else if (age>=12 && age<18){
-    console .log("Teen ticket: $7");
-}
-else if (age>=18 && age<=60){
-    console .log("Adult ticket: $10");
-}
-else{
-    console .log("Senior ticket: $10");
-}
+let shippingCost = (orderAmount <= 100) ? "Free Shipping" : "Shipping Cost $10";
+
+console .log(shippingCost)
