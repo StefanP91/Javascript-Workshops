8f8de7caba833d9246edaa540674e5bb8678eb06
Stefan Panov <64032523+StefanP91@users.noreply.github.com>
2024-05-25 22:32:49 +0200

    Adding folder Workshop 2

---
+++ Workshop 2/index.js
@@ -0,0 +1,12 @@
+// Task 1
+
+let email = "           TEST@test.com"
+
+if (email.startsWith(" ")) {
+    console.log(email.trim() .toLocaleLowerCase())
+}
+
+else{
+    console .log(email.toLocaleLowerCase)
+}
+
